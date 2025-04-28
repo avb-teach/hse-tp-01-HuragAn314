bd609e8413035201eb9273f1181e0a02d92da5b5
root <root@ayktfsrbzh.local>
2025-04-28 12:29:40 +0000

    test

--- collect_files.py
+++ collect_files.py
@@ -10,3 +10,4 @@ for d, i1, fs in os.walk(input_dir):
         while os.path.exists(p:=f"{output_dir}/{b}_{n}{e}" if n else f"{output_dir}/{b}{e}"): n += 1
         k[b]= n+ 1
         s.copy2(f"{d}/{f}", p)
+        
