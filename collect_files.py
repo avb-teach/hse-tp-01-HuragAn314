d294d9466fedccf9868d72c39c484ebdd198901e
root <root@ayktfsrbzh.local>
2025-04-28 13:47:52 +0000

    test

--- collect_files.py
+++ collect_files.py
@@ -1,15 +1,14 @@
 import os, sys, shutil as s
 
-input_dir, output_dir = sys.argv[1], sys.argv[2]
-mx=int(sys.argv[4]) if '--max_depth' in sys.argv else None
+input_dir, output_dir =sys.argv[1], sys.argv[2]
+mx=int(sys.argv[sys.argv.index('--max_depth')+1]) if '--max_depth' in sys.argv else None
 os.makedirs(output_dir, exist_ok=True)
-k = {}
-for d, i1, f in os.walk(input_dir):
-    r=os.path.relpath(d, input_dir)
-    l=r.split(os.sep).index('.') if r=='.' else len(r.split(os.sep))
-    if mx is not None and l>mx: continue
-    for i in f:
-        a, x=os.path.splitext(i)
+k={}
+for d, i1, fs in os.walk(input_dir):
+    if mx and (p:=os.path.relpath(d, input_dir))!='.' and p.count(os.sep)>mx: continue
+    for f in fs:
+        a, x= os.path.splitext(f)
         n =k.get(a, 0)
-        while os.path.exists(p:=f"{output_dir}/{a}_{n}{x}" if n else f"{output_dir}/{a}{x}"): n+=1
-        k[a], i1 =n+1, s.copy2(os.path.join(d, i), p)
+        while os.path.exists(dx:=os.path.join(output_dir, f"{a}_{n}{x}") if n else os.path.join(output_dir, f"{a}{x}")):n+=1
+        k[a] =n+1
+        s.copy2(os.path.join(d, f), dx)
