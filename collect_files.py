e1791d2cd8015a8bbf4ece17b6cf909b05e1e8cc
root <root@ayktfsrbzh.local>
2025-04-28 12:28:23 +0000

    test

--- collect_files.py
+++ collect_files.py
@@ -6,7 +6,7 @@ k = {}
 for d, i1, fs in os.walk(input_dir):
     for f in fs:
         b, e =os.path.splitext(f)
-        n =c.get(b, 0)
+        n =k.get(b, 0)
         while os.path.exists(p:=f"{output_dir}/{b}_{n}{e}" if n else f"{output_dir}/{b}{e}"): n += 1
         k[b]= n+ 1
         s.copy2(f"{d}/{f}", p)
